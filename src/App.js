ad7952813ac45fb3a9f27d83ecff6e5c0fd574a7
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2023-12-08 07:32:13 +0100

    feat:implementation de translation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import English from "./translation/en.json";
+import French from "./translation/fr.json";
+import Spanish from "./translation/es.json";
+import German from "./translation/de.json";
+import Italy from "./translation/it.json";
+import Hindi from "./translation/hi.json";
+import Turkish from "./translation/tr.json";
+import Arabe from "./translation/ar.json";
 
+import { useTranslation } from 'react-i18next';
 function App() {
+  const { t, i18n } = useTranslation();
+  const [locale, setLocale] = useState('en');
+  const [lang, setLang] = useState(English);
+  const changeLang = getLang => {
+    setLocale(getLang.target.value);
+    i18n.changeLanguage(getLang.target.value);
+    switch (getLang) {
+      case 'en':
+        setLang(English)
+        break;
+      case 'fr':
+        setLang(French)
+        break;
+      case 'es':
+        setLang(Spanish)
+        break;
+      case 'de':
+        setLang(German)
+        break;
+      case 'it':
+        setLang(Italy)
+        break;
+      case 'hi':
+        setLang(Hindi)
+        break;
+      case 'ar':
+        setLang(Arabe)
+        break;
+      case 'tr':
+        setLang(Turkish)
+        break;
+      default:
+        setLang(English);
+    }
+
+  }
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select name="pets" id="pet-select" onChange={changeLang}>
+        <option value="">--Please choose an option--</option>
+        <option value="en">English</option>
+        <option value="fr">French</option>
+        <option value="es">Spanish</option>
+        <option value="de">German</option>
+        <option value="it">Italy</option>
+        <option value="hi">Hindi</option>
+        <option value="ar">Arabe</option>
+        <option value="tr">Turkish</option>
+      </select>
+      <div>
+        {lang.translation.fact.map((item, key) => (
+          <div>
+            <h1>{item.title}</h1>
+            <h1>{item.description}</h1>
+          </div>
+        ))}
+
+
+      </div>
+      <h1>{t('wakeup')}</h1>
     </div>
   );
 }
---
+++ src/i18n.js
@@ -0,0 +1,28 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+
+const resources = {
+    en: require('./translation/en.json'),
+    fr: require('./translation/fr.json'),
+    es: require('./translation/es.json'),
+    de: require('./translation/de.json'),
+    it: require('./translation/it.json'),
+    ar: require('./translation/ar.json'),
+    hi: require('./translation/hi.json'),
+    tr: require('./translation/tr.json'),
+};
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources,
+        lng: `${navigator.language.split('-')[0] || navigator.userLanguage('-')[0]}`,
+        fallbackLng: `${navigator.language || navigator.userLanguage}`,
+
+        interpolation: {
+            escapeValue: false
+        }
+    });
+
+export default i18n;
